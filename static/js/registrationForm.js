7fa0dc5a3a080b512911cbb0649433fb6b510fe7
Digist <55279565+Digist@users.noreply.github.com>
2020-05-02 05:48:32 -0700

    Update registrationForm.js

--- static/js/registrationForm.js
+++ static/js/registrationForm.js
@@ -7,7 +7,7 @@
         window.scrollTo(0,0);
         fetchData('/candidatearea/registration',function(data){
             
-            code.value =  ${data.id};
+            code.value = data.id ;
             var $form = $('form#registration-form'),
                 
             url = 'https://script.google.com/macros/s/AKfycbyvJRlJ7IRg0ayR8yZzo2XSwcvzGDKnjei1PGLcSxLKxFtvnB6K/exec'
