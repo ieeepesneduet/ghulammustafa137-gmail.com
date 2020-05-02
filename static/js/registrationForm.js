e763e657915af21f4165716203c9ca2781cb25aa
G-Mustafa <46380399+G-Mustafa@users.noreply.github.com>
2020-05-03 00:57:01 +0500

    Update registrationForm.js

--- static/js/registrationForm.js
+++ static/js/registrationForm.js
@@ -6,36 +6,6 @@
         e.preventDefault();
         window.scrollTo(0,0);
         fetchData('/candidatearea/registration',function(data){
-            
-            code.value = 'pes/20/' + data.id ;
-            var $form = $('form#registration-form'),
-                
-            url = 'https://script.google.com/macros/s/AKfycbyvJRlJ7IRg0ayR8yZzo2XSwcvzGDKnjei1PGLcSxLKxFtvnB6K/exec'
-            
-                $.fn.serializeObject = function()
-                {
-                var o = {};
-                var a = this.serializeArray();
-                $.each(a, function() {
-                if (o[this.name]) {
-                if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value );
-                } else {
-                o[this.name] = (this.value );
-                }
-                });
-                return o;
-                };
-            
-               var jqxhr = $.ajax({
-                  url: url,
-                  method: "GET",
-                  dataType: "json",
-                  data: $form.serializeObject()
-               });
-           
             showMsg(`Your application has been received.Your application id is pes/20/${data.id} .You can use this id to check the status of your application.Your code will be emailed to you shortly yy.`,'primary')
             registrationForm.reset();
         },new PostFormData(registrationForm))
